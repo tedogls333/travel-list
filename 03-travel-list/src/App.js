9dbac627ef574e4f7522211965f8607393a22b7e
tedogls333 <tedogls3@gmail.com>
2024-02-28 14:58:20 +0100

    travel-list

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer>
+      <em>💼 you have X items on your list, And you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
