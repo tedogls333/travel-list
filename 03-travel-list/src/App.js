e326eede58d98e04d67e6e5380a4590c750b5fd4
tedogls333 <tedogls3@gmail.com>
2024-03-03 19:46:53 +0100

    travel-list

--- 03-travel-list/src/App.js
+++ 03-travel-list/src/App.js
@@ -1,11 +1,5 @@
 import { useState } from 'react';
 
-const initialItems = [
-  { id: 1, description: 'Passports', quantity: 2, packed: false },
-  { id: 2, description: 'Socks', quantity: 12, packed: true },
-  { id: 3, description: 'Charger', quantity: 1, packed: false },
-];
-
 function App() {
   const [items, setItems] = useState([]);
 
@@ -37,7 +31,6 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     onAddItems(newItem);
 
