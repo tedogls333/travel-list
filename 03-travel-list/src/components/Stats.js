d2460e4bc75bcf671c0b60eb73c6611724baaa10
tedogls333 <tedogls3@gmail.com>
2024-03-05 22:17:16 +0100

    travel-list

---
+++ 03-travel-list/src/components/Stats.js
@@ -0,0 +1,21 @@
+export default function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items on your packing list 🚀</p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `💼 you have ${numItems} items on your list, And you already packed{' '}
+        {numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
